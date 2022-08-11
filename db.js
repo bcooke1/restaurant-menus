fb6e49a6ef19c1e6365fda6639d7e9a22c561a1b
Ben Cooke <bencooke.thedrifthouse@gmail.com>
2022-08-11 11:07:45 +0100

    menu and resturant done

--- db.js
+++ db.js
@@ -3,6 +3,16 @@ const { Sequelize } = require('sequelize');
 
 // TODO - connect to db via sequelize
 
+const db = new Sequelize({
+    dialect: 'sqlite',
+    storage: './data.sqlite'
+})
+
+
+//
+
+
+
 module.exports = {
-    sequelize
+    db
 };
--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,6 +1,31 @@
-const {sequelize} = require('../db');
-const { Sequelize } = require('sequelize');
+const {db} = require('../db');
+const { Sequelize, DataTypes } = require('sequelize');
 
 // TODO - create a Restaurant model
+
+const Restaurant = db.define('restaurant',{
+    name: {
+        type: DataTypes.STRING
+    },
+    location: {
+        type:DataTypes.STRING
+    },
+    cuisine: {
+        type:DataTypes.STRING
+    }
+}
+)
+
+async function main(){
+    await Restaurant.sync()
+
+    await Restaurant.create({
+        name: 'Mcdonalds',
+        location: 'London',
+        cuisine: 'American'
+    })
+}
+
+//
 
 module.exports = {Restaurant};
